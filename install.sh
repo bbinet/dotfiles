1d64644c83668cbea284d8f94c9a2528a11b2b23
Bruno Binet <bruno.binet@gmail.com>
2015-01-22 23:28:02 +0100

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export CWD="$( cd "$( dirname "$0" )" && pwd )"
+
+for d in $(ls "${CWD}/dot")
+do
+    rm -f ~/.${d}
+    ln -sf "${CWD}/dot/${d}" ~/.${d}
+done
+
+if [ ! -f ~/.pip_bootstrap_profile.sh ]
+then
+    ./pip/bootstrap_pip2014.sh
+fi
+
+if [ ! -d ~/.virtualenvs/syntax-checkers ]
+then
+    source ~/.pip_bootstrap_profile.sh
+    mkvirtualenv syntax-checkers
+    pip install -r pip/syntax-checkers.txt
+    deactivate
+fi
