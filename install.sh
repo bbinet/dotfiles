a6782614cc000cfcb61271569d6bc4f08617763b
Bruno Binet <bruno.binet@gmail.com>
2017-01-15 01:08:55 +0100

    remove bootstrap_pip2014.sh, deactivate YouCompleteMe

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ do
     ln -sf "${CWD}/dot/${d}" ~/.${d}
 done
 
-if [ ! -f ~/.pip_bootstrap_profile.sh ]
+if [ ! -f /usr/local/bin/virtualenvwrapper.sh ]
 then
-    ./pip/bootstrap_pip2014.sh
+    echo "Virtualenvwrapper must be installed first"
+    exit 1
 fi
 
 if [ ! -d ~/.virtualenvs/syntax-checkers ]
 then
-    source ~/.pip_bootstrap_profile.sh
+    source /usr/local/bin/virtualenvwrapper.sh
     mkvirtualenv syntax-checkers
     pip install -r pip/syntax-checkers.txt
     deactivate
@@ -25,7 +26,8 @@ if [ "$1" == "-a" ]
 then
     if [ ! -f ~/.bin/hub ]
     then
-       curl -L https://github.com/github/hub/releases/download/v2.2.1/hub-linux-amd64-2.2.1.tar.gz | tar zx --strip=1 -C ~/.bin/ hub-linux-amd64-2.2.1/hub
+       echo "fixme download hub"
+       #curl -L https://github.com/github/hub/releases/download/v2.2.1/hub-linux-amd64-2.2.1.tar.gz | tar zx --strip=1 -C ~/.bin/ hub-linux-amd64-2.2.1/hub
     fi
 fi
 
