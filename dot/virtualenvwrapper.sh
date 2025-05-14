dfa6737a398222a203e88470b3d69bdbb5ac967e
Bruno Binet <bruno.binet@gmail.com>
2025-05-14 09:54:02 +0200

    Update for debian 13

--- dot/virtualenvwrapper.sh
+++
@@ -1,6 +0,0 @@
-# protect from modified PATH that may contain virtualenv bin path
-if [ -f ~/.local/bin/virtualenvwrapper.sh ]; then
-    export PATH=${PATH/$PREPEND_PATH:/}
-    source ~/.local/bin/virtualenvwrapper.sh
-    export PATH=$PREPEND_PATH:$PATH
-fi
--- install.sh
+++ install.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-export CWD="$( cd "$( dirname "$0" )" && pwd )"
+# sudo apt install pipx virtualenvwrapper
 
+export CWD="$( cd "$( dirname "$0" )" && pwd )"
+BACKUP_DIR="${CWD}/dot/tmp/$(date +%Y%m%d_%H%M%S)"
+
+# TODO create new backup dir before replacing dotfiles
 for d in $(ls "${CWD}/dot")
 do
-    rm -f ~/.${d}
-    ln -sf "${CWD}/dot/${d}" ~/.${d}
+    if [ "$(realpath ~/.${d})" != "${CWD}/dot/${d}" ]
+    then
+        mkdir -p $BACKUP_DIR
+        mv ~/.${d} $BACKUP_DIR
+        ln -sf "${CWD}/dot/${d}" ~/.${d}
+    fi
 done
 
 if [ "$1" == "-a" ]
 then
-    (dpkg -l | grep -q python-pip) && sudo apt purge python-pip
-    echo "For Python3:"
-    sudo apt install python3-distutils
-    wget -O /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
-    sudo python3 /tmp/get-pip.py --upgrade --force-reinstall
-    pip3 install --upgrade --user virtualenvwrapper
-    pip3 install --upgrade --user pipenv
-    echo "For Python2:"
-    sudo apt install python-distutils
-    wget -O /tmp/get-pip.py https://bootstrap.pypa.io/pip/2.7/get-pip.py
-    sudo python2 /tmp/get-pip.py --upgrade --force-reinstall
-    pip2 install --upgrade --user virtualenvwrapper
-    pip2 install --upgrade --user pipenv
-    if [ ! -f ~/.bin/hub ]
-    then
-        echo "Installing hub..."
-       curl -L https://github.com/github/hub/releases/download/v2.12.1/hub-linux-amd64-2.12.1.tgz | tar zx --strip=2 -C ~/.bin/ hub-linux-amd64-2.12.1/bin/hub
-    fi
+    sudo apt install pipx virtualenvwrapper
 fi
 
 vim +PlugInstall +qall
